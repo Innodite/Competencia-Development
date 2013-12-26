e2d2c3e1f6b1eaac5c0ed18a2607f7b98f8117b1
Anthony Filgueira <Anthony Filgueira@192.168.0.101>
2013-12-26 18:28:09 -0430

    respaldo base de datos

---
+++ backup/querys important.sql
@@ -0,0 +1,20 @@
+﻿CREATE FUNCTION insertar_usuario(pnombre character varying) 
+RETURNS boolean AS $BODY$
+begin
+INSERT INTO usuario(nombre) VALUES (pnombre);
+if found then
+return true;
+else
+return false;
+end if;
+end;$BODY$
+LANGUAGE 'plpgsql'
+
+CREATE OR REPLACE VIEW ver_usuario AS 
+SELECT id,nombre from usuario where nombre='anthony'
+
+select nombre FROM ver_usuario
+
+alter table competidor
+ add constraint NOMBRECONSTRAIN
+ check (char_length(trim(nombre))<>0)
